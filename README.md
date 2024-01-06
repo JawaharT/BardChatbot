d7775c8ff7adce675fccda406307caffadf816f2
Jawahar <jtunuguntla@gmail.com>
2024-01-06 16:31:41 +0000

    Added comments in main and readme

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # BardChatbot
 
+Inside main.py cookie_dict provide the necessary cookies using the steps below:
+
 > **Warning** Do not expose the `__Secure-1PSID` 
 1. Visit https://bard.google.com/
 2. F12 for console
 3. Session: Application → Cookies → Copy the value of  `__Secure-1PSID`, `_Secure-1PSIDTS`, `__Secure-1PSIDCC` cookies.
 
-Note that while I referred to `__Secure-1PSID` value as an API key for convenience, it is not an officially provided API key. 
+Note that while I referred to `__Secure-1PSID`, `_Secure-1PSIDTS`, `__Secure-1PSIDCC` values as an API key for convenience, it is not an officially provided API key. 
 Cookie value subject to frequent changes. Verify the value again if an error occurs. Most errors occur when an invalid cookie value is entered.
+
+Once cookies provided:
+Run App using command: streamlit run main.py
--- main.py
+++ main.py
@@ -2,38 +2,44 @@ from bardapi import BardCookies
 import streamlit as st
 from streamlit_chat import message
 
+# cookies stored in this dictionary
 cookie_dict = {
-    "__Secure-1PSID": ".",
-    "__Secure-1PSIDTS": "",
-    "__Secure-1PSIDCC": ""
+    "__Secure-1PSID": "XXXXXXXX",
+    "__Secure-1PSIDTS": "XXXXXXXX",
+    "__Secure-1PSIDCC": "XXXXXXXX"
 }
 
+# Streamlit session state to store the chat converations
 if 'generate' not in st.session_state:
     st.session_state['generate'] = []
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
+# bard object creation
 bard = BardCookies(cookie_dict=cookie_dict)
-#message = input("Enter your prompt: ")
-#print(bard.get_answer(message)['content'])
 
 st.title("Google Bard Chatbot")
 
+# get bard response
 def response_api(prompt):
     message = bard.get_answer(prompt)['content']
     return message
 
+# get user prompt
 def user_input():
     input_text = st.text_input("Enter Your Prompt: ")
     return input_text
 
 user_text = user_input()
+
+# if user prompt provided add to session state of Streamlit app
 if user_text:
     output = response_api(user_text)
     st.session_state.generate.append(output)
     st.session_state.past.append(user_text)
 
+# show message to user after bard response generated
 if st.session_state['generate']:
     for i in range(len(st.session_state['generate'])-1, -1, -1):
         message(st.session_state['past'][i], is_user=True, key=str(i) + "_user")
