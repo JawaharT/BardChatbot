13ed440072dec28e2b49664a5d4d5b7962af7552
tunugunm <tunugunm@visa.com>
2024-01-06 15:33:34 +0000

    main app

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 from bardapi import BardCookies
 import streamlit as st
 from streamlit_chat import message
-import os
 
 cookie_dict = {
-    "__Secure-1PSID": "",
+    "__Secure-1PSID": ".",
     "__Secure-1PSIDTS": "",
-    "__Secure-1PSIDCC":""
+    "__Secure-1PSIDCC": ""
 }
 
-# cookie_dict = {
-#     "__Secure-1PSID": os.environ.get("secure-1PSI"),
-#     "__Secure-1PSIDTS": os.environ.get("secure-1PSIDTS"),
-#     "__Secure-1PSIDCC": os.environ.get("secure-1PSIDCC"),
-# }
-
 if 'generate' not in st.session_state:
-    st.session_state['generate']=[]
+    st.session_state['generate'] = []
 
 if 'past' not in st.session_state:
-    st.session_state['past']=[]
+    st.session_state['past'] = []
 
 bard = BardCookies(cookie_dict=cookie_dict)
 #message = input("Enter your prompt: ")
@@ -39,7 +32,7 @@ user_text = user_input()
 if user_text:
     output = response_api(user_text)
     st.session_state.generate.append(output)
-    st.session_state.generate.append(user_text)
+    st.session_state.past.append(user_text)
 
 if st.session_state['generate']:
     for i in range(len(st.session_state['generate'])-1, -1, -1):
